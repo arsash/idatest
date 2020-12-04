1fdde55879734592f75d052f6e4d2d84c36bc4c1
Arshaluys Aslanyan <a.aslanyan.a@gmail.com>
2020-12-05 00:36:34 +0300

    delete routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
     <header-block/>
-    <rent-block>
-      <RentItem>dfgdfgdfg</RentItem>
-    </rent-block>
+    <rent-block></rent-block>
   </div>
 </template>
 
 <script>
 import HeaderBlock from './components/header.vue'
 import RentBlock from './components/rent.vue'
-
-
 export default {
   name: 'App',
   components: {
